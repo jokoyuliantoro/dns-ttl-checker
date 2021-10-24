aa75ace7b0984d96bb31bc1b44e118420d538af9
jky <joko.yuliantoro@gmail.com>
2021-10-24 21:31:50 +0700

    Add pre-check and usleep
    1) Add pre-check to get ANSWER count. Don't waste +trace if ANSWER is 0.
    2) Add usleep to delay the checking process per domain.

--- digdown.pl
+++ digdown.pl
@@ -1,33 +1,45 @@
 #!/usr/bin/perl
+use Time::HiRes qw(usleep);
+
 $|=1;
 while (<>) {
   chomp;
-  $dcheck = $_;
-  $notfound = 1;
-  do {
-    $ttl = "";
-    $ips = "";
-    $dom = "";
-    open F, "dig +trace $dcheck |";
-    while (<F>) {
-      if (/^(\S+)\s+([0-9]+)\s+IN\s+A+\s+(\S+)/) {
-        $dom = $1;
-        $ttl = $2;
-        $ips = $3;
-        $notfound = 0;
-      } elsif (/\s+IN\s+CNAME\s+(\S+)/) {
-        $dom = $1;
-      }
+  $dori = $_;
+  $found = 1;
+  open F, "dig $dori | grep ANSWER |" or die("$dori;;nottl;noanswer");
+  while (<F>) {
+    if (/ANSWER\: 0,/) {
+      $found = 0;
     }
-    close F;
-    if ($dom eq "") {
-      $notfound = 0;
-    } else {
-      $dcheck = $dom;
-    }
-  } while $notfound;
+  }
+  close F;
 
-  $ttl = "nottl" if $ttl eq "";
-  $ips = "noanswer" if $ips eq "";
-  print "$dcheck;$dom;$ttl;$ips\n";
+  if ($found == 1) {
+    $not_address = 1;
+    $dcheck = $dori;
+    do {
+      $dom = "";
+      open F, "dig +trace $dcheck |";
+      while (<F>) {
+        if (/^(\S+)\s+([0-9]+)\s+IN\s+A+\s+(\S+)/) {
+          $dom = $1;
+          $ttl = $2;
+          $ips = $3;
+          $not_address = 0;
+        } elsif (/\s+IN\s+CNAME\s+(\S+)/) {
+          $dom = $1;
+        }
+      }
+      close F;
+      if ($dom eq "") {
+        $not_address = 0;
+      } else {
+        $dcheck = $dom;
+      }
+    } while $not_address;
+    print "$dori;$dom;$ttl;$ips\n";
+  } else {
+    print "$dori;;nottl;noanswer\n";
+  }
+  usleep(250000);
 }
